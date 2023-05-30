f1c2f55fc6ba00fabea924544a955d71346e2da6
Penny Hancock <pahanc@dide.local>
2023-05-30 15:49:13 +0100

    updates to parameters.R

--- R/parameters.R
+++ R/parameters.R
@@ -235,7 +235,7 @@
 #' @export
 get_parameters <- function(overrides = list(),square_number=square_number,run_number=run_number) {
   
-  supp_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 stoch12/mosq_supp_het_stronger_gamb",square_number,"_",run_number,".csv",sep="")
+  supp_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 stoch/mosq_supp_het_stronger_gamb",square_number,"_",run_number,".csv",sep="")
   mosq_suppression_gamb<-unlist(read.csv(supp_filename_gamb,header=F,colClasses="numeric"))
   #mosq_suppression<-unlist(read.csv("/Imperial March 2021/SEA project/mosq_suppression.csv",header=F,colClasses="numeric"))
   dimnames(mosq_suppression_gamb)<-NULL
@@ -245,7 +245,7 @@ get_parameters <- function(overrides = list(),square_number=square_number,run_nu
   
   ###mosq_suppression_gamb<-rep(1,(29*365))
 
-  supp_filename_arab<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 stoch12/mosq_supp_het_stronger_arab",square_number,"_",run_number,".csv",sep="")
+  supp_filename_arab<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 stoch/mosq_supp_het_stronger_arab",square_number,"_",run_number,".csv",sep="")
   mosq_suppression_arab<-unlist(read.csv(supp_filename_arab,header=F,colClasses="numeric"))
   dimnames(mosq_suppression_arab)<-NULL
   mosq_suppression_arab<-as.vector(mosq_suppression_arab)
@@ -254,7 +254,7 @@ get_parameters <- function(overrides = list(),square_number=square_number,run_nu
 
   ###mosq_suppression_arab<-rep(1,(29*365))
   
-  supp_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 stoch12/mosq_supp_het_stronger_fun",square_number,"_",run_number,".csv",sep="")
+  supp_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 stoch/mosq_supp_het_stronger_fun",square_number,"_",run_number,".csv",sep="")
   mosq_suppression_fun<-unlist(read.csv(supp_filename_fun,header=F,colClasses="numeric"))
   dimnames(mosq_suppression_fun)<-NULL
   mosq_suppression_fun<-as.vector(mosq_suppression_fun)
@@ -268,17 +268,17 @@ get_parameters <- function(overrides = list(),square_number=square_number,run_nu
   mosq_supp_lst[[2]]<-mosq_suppression_arab
   mosq_supp_lst[[3]]<-mosq_suppression_fun
 
-  seas_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 Emerge12/mosq_seasonality_gamb",square_number,".csv",sep = "")
+  seas_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 Emerge/mosq_seasonality_gamb",square_number,"_",run_number,".csv",sep = "")
   mosq_seasonality_gamb<-unlist(read.csv(seas_filename_gamb,header=F,colClasses="numeric"))
   dimnames(mosq_seasonality_gamb)<-NULL
   mosq_seasonality_gamb<-as.vector(mosq_seasonality_gamb)
 
-  seas_filename_arab<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 Emerge12/mosq_seasonality_arab",square_number,".csv",sep = "")
+  seas_filename_arab<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 Emerge/mosq_seasonality_arab",square_number,"_",run_number,".csv",sep = "")
   mosq_seasonality_arab<-unlist(read.csv(seas_filename_arab,header=F,colClasses="numeric"))
   dimnames(mosq_seasonality_arab)<-NULL
   mosq_seasonality_arab<-as.vector(mosq_seasonality_arab)
 
-  seas_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 Emerge12/mosq_seasonality_fun",square_number,".csv",sep = "")
+  seas_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 Emerge/mosq_seasonality_fun",square_number,"_",run_number,".csv",sep = "")
   mosq_seasonality_fun<-unlist(read.csv(seas_filename_fun,header=F,colClasses="numeric"))
   dimnames(mosq_seasonality_fun)<-NULL
   mosq_seasonality_fun<-as.vector(mosq_seasonality_fun)
