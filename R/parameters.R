e5859b1b251c52f964ee95f36333bd2f11e47c3d
Penny Hancock <pahanc@dide.local>
2023-07-06 22:25:39 +0100

    updates to parameters.R and vector parameters.R

--- R/parameters.R
+++ R/parameters.R
@@ -269,10 +269,11 @@ get_parameters <- function(overrides = list(),square_number=square_number,run_nu
   mosq_supp_lst[[1]]<-mosq_suppression_gamb
   mosq_supp_lst[[2]]<-mosq_suppression_arab
   mosq_supp_lst[[3]]<-mosq_suppression_fun
-  mosq_supp_lst[[4]]<-mosq_suppression_new
+  #mosq_supp_lst[[4]]<-mosq_suppression_new
   
 
-  seas_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 Emerge/mosq_seasonality_gamb",square_number,"_",run_number,".csv",sep = "")
+  #seas_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/May 23 Emerge/mosq_seasonality_gamb",square_number,"_",run_number,".csv",sep = "")
+  seas_filename_gamb<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Apr 23 Emerge12/mosq_seasonality_gamb",square_number,".csv",sep = "")
   mosq_seasonality_gamb<-unlist(read.csv(seas_filename_gamb,header=F,colClasses="numeric"))
   dimnames(mosq_seasonality_gamb)<-NULL
   mosq_seasonality_gamb<-as.vector(mosq_seasonality_gamb)
@@ -296,7 +297,7 @@ get_parameters <- function(overrides = list(),square_number=square_number,run_nu
   mosq_seas_lst[[1]]<-mosq_seasonality_gamb
   mosq_seas_lst[[2]]<-mosq_seasonality_arab
   mosq_seas_lst[[3]]<-mosq_seasonality_fun
-  mosq_seas_lst[[4]]<-mosq_seasonality_new
+  #mosq_seas_lst[[4]]<-mosq_seasonality_new
 
 
   parameters <- list(
